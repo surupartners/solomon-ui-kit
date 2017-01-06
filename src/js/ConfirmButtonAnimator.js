49c2bc159dda644a1e9f4dd1c4d990b51b5d91ba
Matt Cholawo <m.cholawo@gmail.com>
2017-01-06 15:56:18 +0000

    Adds doc blocks

--- src/js/ConfirmButtonAnimator.js
+++ src/js/ConfirmButtonAnimator.js
@@ -5,7 +5,36 @@
 var $ = require('jquery'),
     _ = require('underscore');
 
+/**
+ * The user can click on a button to reveal an element containing confirm and cancel buttons.  The cancel button resets
+ * the user interaction, where the confirm button (usually) submits a form.  Anything else can be placed in the element
+ * which is revealed, such as help text to explain what will happen, or a confirmation question such as "Are you sure?".
+ *
+ * <button class="js--confirm-button">Delete this thing</button>
+ *
+ * <div class="js--confirm-button__wrapper">
+ *     Are you sure?
+ *     <Form>
+ *     <button class="js--confirm-button__confirm">Yes Delete this</button>
+ *     </Form>
+ *     <button class="js--confirm-button__cancel">No, cancel</button>
+ * </div>
+ *
+ * @type {Object}
+ */
 var ConfirmButtonAnimator = {
+
+    registerListeners: function () {
+        $(document).on('click', '.js--confirm-button', _.bind(function (event) {
+            event.preventDefault();
+            this.revealConfirmButton(event.currentTarget);
+        }, this));
+
+        $(document).on('click', '.js--confirm-button__cancel', _.bind(function (event) {
+            event.preventDefault();
+            this.hideConfirmButton(event.currentTarget);
+        }, this));
+    }
 
     revealConfirmButton: function (revealButton) {
         var $buttonWrapper  = $(revealButton).parents('.js--confirm-button__wrapper'),
@@ -30,19 +59,6 @@ var ConfirmButtonAnimator = {
 
         $buttonWrapper.find(".js--confirm-button__confirm, .js--confirm-button__cancel").prop("disabled", true);
     },
-
-    registerListeners: function () {
-        $(document).on('click', '.js--confirm-button', _.bind(function (event) {
-            event.preventDefault();
-            this.revealConfirmButton(event.currentTarget);
-        }, this));
-
-        $(document).on('click', '.js--confirm-button__cancel', _.bind(function (event) {
-            event.preventDefault();
-            this.hideConfirmButton(event.currentTarget);
-        }, this));
-    }
-
 };
 
 module.exports = ConfirmButtonAnimator;
--- src/js/DisabledUnlessHasCheckedElements.js
+++ src/js/DisabledUnlessHasCheckedElements.js
@@ -6,7 +6,18 @@ var $       = require('jquery'),
     _       = require('underscore'),
     Helpers = require('./Helpers');
 
-
+/**
+ * Sets the disabled property on an element unless some of its associated checkboxes are checked.
+ * Typical use case is to stop the user from submitting a form without having selected any checkboxes.
+ * Call the registerListner method passing some kind of hook for the element that jQuery can hook into (generally a css class)
+ * Give the element data-target-element attribute to point to its associated checkboxes. E.g.
+ *
+ * <button class="js--disabled-unless-has-checked-elements" data-target-element=".js--target-checkbox-identifier">Click me</button>
+ * <input type="checkbox" class="js--target-checkbox-identifier" />
+ *
+ * This JS will immediately disable the button if no associated checkboxes are checked, and binds to checkbox change event to
+ * re-enable the button if any checkboxes are checked.
+ */
 var DisabledUnlessHasCheckedElements = {
     values: {
         $elementsToListenTo: null,
